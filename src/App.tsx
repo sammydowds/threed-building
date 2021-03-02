1e66c0514270325f23a18836a9682cf8163c989e
sammydowds <sammydowds1993@gmail.com>
2021-03-01 18:51:31 -0600

    feat: adding 3d box

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Box } from './features/building/Building'; 
 import './App.css';
+import { Canvas } from 'react-three-fiber'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+	<Canvas>
+	      <Box />
+	</Canvas>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
